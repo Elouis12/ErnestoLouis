f7f3f391821e8128bff1ecf71de7954df1442fa6
Ernesto Louis <elouis12@me.com>
2022-08-28 20:55:32 -0400

    saved theme to localstorage

--- js/index.js
+++ js/index.js
@@ -76,6 +76,8 @@ const projectDisplay = (project) => {
 }
 
 
+let darkModeTheme = true; // dark mode initially
+
 const darkMode = () => {
 
 
@@ -83,15 +85,44 @@ const darkMode = () => {
 
     let themeIcons = document.getElementsByClassName("themeIcon");
 
-    for( let x = 0; x < themeIcons.length; x+=1 ){ themeIcons[x].classList.toggle("darkModeIcon") }
+    for( let x = 0; x < themeIcons.length; x+=1 ){
+
+        themeIcons[x].classList.toggle("darkModeIcon");
+
+    }
+
+    let titles = document.getElementsByClassName("section-title");
+
+    for( let x = 0; x < titles.length; x+=1 ){
+
+        titles[x].classList.toggle("dark-mode-section-title");
+
+    }
+
 
     let nightTheme = document.getElementById("nightTheme");
 
     nightTheme.getAttribute("class").includes("fa-cloud-moon") ?
-        nightTheme.setAttribute("class", "fa-solid fa-cloud-sun") :
-        nightTheme.setAttribute("class", "fa-solid fa-cloud-moon")
+    nightTheme.setAttribute("class", "fa-solid fa-cloud-sun") :
+    nightTheme.setAttribute("class", "fa-solid fa-cloud-moon")
 
     let logo = document.getElementById("logo");
     logo.style.color = "hsl(0, 0%, 100%)";
 
+
+    darkModeTheme = !darkModeTheme;
+
+    localStorage.setItem("darkMode", darkModeTheme);
+
+}
+
+
+const setTheme = () => {
+
+    let lightMode = localStorage.getItem("darkMode");
+    
+    if( lightMode === "false" ){
+
+        darkMode();
+    }
 }
